45702823ad4508ab96884ff3f3ad1657ab45a2ed
Amit Gupta <amit@gprof.com>
2020-09-05 10:47:08 -0700

    Initial Commit

---
+++ GuessingGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GuessingGame
+//
+//  Created by Amit Gupta on 9/5/20.
+//  Copyright © 2020 AI Club. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
